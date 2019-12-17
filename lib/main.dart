568b116d29cdaea6b69d95f2df8bcc0014aa5272
Zodaztream <felno889@student.liu.se>
2019-12-17 02:06:40 +0100

    Carousel almost done

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-  final List<List<dynamic>> objects = [["image1", "title1"]];
+  // Below is a test list
+  final List<List<dynamic>> objects = [["assets/flutter.jpg", "title1"], ["assets/flutter.jpg", "title2"], ["assets/flutter.jpg", "title3"]
+  , ["assets/flutter.jpg", "title4"], ["assets/flutter.jpg", "title5"], ["assets/flutter.jpg", "title6"], ["assets/flutter.jpg", "title7"]
+  ,["assets/flutter.jpg", "title8"] ,["assets/flutter.jpg", "title9"] ,["assets/flutter.jpg", "title10"]];
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Carousel(
-        amount: 10,
+        amount: 5,
         displayType: DisplayType.HORIZONTAL,
         itemBuilder: CarouselObject.objectBuilder(objects),
       ),
@@ -23,11 +26,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// required amount : How many items to show at a time
+/// required displayType: defines how to display images (vertically or horizontally)
+/// required itemBuilder: list of display items
+/// of format [['image','title',(Optional)component],...]
+/// canClick : defines if items should be clicked
+/// Size : Define the size of the display image
 class Carousel extends StatefulWidget {
-  final int amount;
   final DisplayType displayType;
   final List<CarouselObject> itemBuilder;
   final bool canClick;
+  final Size size;
+  final int amount;
 
   Carousel({
     Key key,
@@ -35,6 +45,7 @@ class Carousel extends StatefulWidget {
     @required this.displayType,
     @required this.itemBuilder,
     this.canClick = false,
+    this.size = const Size(16, 16),
   }) : super(key: key);
 
   @override
@@ -42,9 +53,83 @@ class Carousel extends StatefulWidget {
 }
 
 class _CarouselState extends State<Carousel> {
+
+  List<List<CarouselObject>> displayObjects = [];
+  int index = 0; // we look at the first set on displayObject
+  // and then we increase index to look at next set.
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+
+    var len = widget.itemBuilder.length;
+    var size = widget.amount;
+    for( int i = 0; i < len; i+=size){
+      int end = i + size < len ? i + size : len;
+      displayObjects.add(widget.itemBuilder.sublist(i, end));
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints ){
+        if(constraints.maxWidth > 300 && constraints.maxHeight > 50){
+          if(widget.displayType == DisplayType.HORIZONTAL){
+            return Center(
+              child: Dismissible(
+                key: ValueKey(index),
+                onDismissed: (DismissDirection direction){
+                  setState((){
+                    index += direction == DismissDirection.endToStart ? 1 : -1;
+                    if(index > displayObjects.length - 1) {
+                      index = 0;
+                    } else if( index < 0) {
+                      index = displayObjects.length - 1;
+                    }
+                  });
+                },
+                // On swipe, increase index (look at next set),
+                child: Container(
+                  color: Colors.white,
+                  width: 500,
+                  height: 300,
+                  child: Row(
+                    children: displayObjects[index].map(
+                  (element) =>
+                    Container(
+                      width: constraints.maxWidth / widget.amount,
+                      child: Column(
+                        children: <Widget>[
+                          Image(
+                          image: AssetImage(element.displayImage),
+                          ),
+                          Text(
+                            element.title
+                          )
+                        ],
+                      ),
+                    )
+                  ).toList(),
+                  )
+                ),
+              ),
+            );
+          }
+          else {
+            return Container(
+              child : Text("Too low terrain")
+            );
+          }
+        }
+        else{
+          return Container();
+        }
+      },
+    );
   }
 }
 
@@ -55,9 +140,9 @@ enum DisplayType {
 
 ///CaourselObject defines each display item.
 ///First argument is the image to display, second is title you want to display,
-///third is its attached component for on-click (optional)
+///third is its attached component to display on-click (optional)
 class CarouselObject {
-  final Image displayImage;
+  final String displayImage;
   final String title;
   var componentElement;
 
