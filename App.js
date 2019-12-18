c9ea186ad76cc62fcfc7bc514188539bb87bda8c
Zodaztream <felno889@student.liu.se>
2019-12-19 00:39:22 +0100

    Fixed appbar

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { StyleSheet, TextInput, Text, View, Button, Image } from 'react-native';
+import React from "react";
+import { StyleSheet, TextInput, Text, View, Button, Image } from "react-native";
+import { Appbar } from "react-native-paper";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image 
-        style={{width: 50, height: 50}}
-        source={require('./assets/icon.png')}>
-          
-        </Image>
+      <Appbar style={styles.bottom}>
+        <Appbar.Content title="Our app" />
+      </Appbar>
+      <Image
+        style={{ width: 128, height: 128 }}
+        source={require("./assets/icon.png")}
+      ></Image>
       <View style={styles.buttonContainer}>
         <Button title="button"></Button>
         <Button title="button"></Button>
@@ -16,24 +19,25 @@ export default function App() {
       <View style={styles.buttonContainer}>
         <Button title="button"></Button>
         <Button title="button"></Button>
-        </View>
-      <View style={{display: "flex", flexDirection: "row"}}>
+      </View>
+      <View style={{ display: "flex", flexDirection: "row" }}>
         <Text>Email</Text>
-        <TextInput  style={{ backgroundColor: '#ededed', height: 40, width: 200 }} value={'Hello'}></TextInput>
+        <TextInput
+          style={{ backgroundColor: "#ededed", height: 40, width: 200 }}
+          value={"Hello"}
+        ></TextInput>
       </View>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     width: "100%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   buttonContainer: {
@@ -42,6 +46,12 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "space-evenly",
     margin: 30
+  },
 
+  bottom: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0
   }
 });
